f73b17019262b1e752301ed1537c3a5fd2ce8f65
Alexey Bondarenko <alexeybondarenko@me.com>
2016-01-22 00:16:51 +0200

    badge to ui-kit

--- src/pages/ui-kit.jsx
+++ src/pages/ui-kit.jsx
@@ -4,7 +4,7 @@ module.exports = React.createClass({
     render: function () {
         return <div>
             <h1>UI Kit</h1>
-            <h2>Buttons</h2>
+            <h2>Button</h2>
             <div>
                 <button className="btn">.btn</button>
                 <div className="btn btn--default">.btn .btn--default</div>
@@ -26,6 +26,28 @@ module.exports = React.createClass({
             <div className="btn-group">
                 <div className="btn btn--default">Read book</div>
                 <div className="btn btn--primary">Buy book</div>
+            </div>
+            <h2>Avatar</h2>
+            <div>
+                <p>
+                    <i>default  </i>
+                    <img src="https://unsplash.it/100/100" className="avatar"/>
+                    <i>  small  </i>
+                    <img src="https://unsplash.it/100/100" className="avatar avatar--small"/>
+                    <i>  medium  </i>
+                    <img src="https://unsplash.it/100/100" className="avatar avatar--medium"/>
+                    <i>  big  </i>
+                    <img src="https://unsplash.it/100/100" className="avatar avatar--big"/>
+                </p>
+            </div>
+            <h2>Badge</h2>
+            <div>
+                some text <div className="badge">badge</div>
+            </div>
+            <h3>Badge group (.badge-group)</h3>
+            <div  className="badge-group">
+                <div className="badge">default</div>
+                <span className="badge badge--primary">.badge--primary</span>
             </div>
         </div>;
     }
